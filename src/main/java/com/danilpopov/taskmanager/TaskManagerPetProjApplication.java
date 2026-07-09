3aefcd3f799d6b469099e2d0306e4116781cd223
Popov Danil <PopovDanil123@gmail.com>
2026-07-09 10:52:18 +0600

    initial commit

---
+++ src/main/java/com/danilpopov/taskmanager/TaskManagerPetProjApplication.java
@@ -0,0 +1,13 @@
+package com.danilpopov.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerPetProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerPetProjApplication.class, args);
+    }
+
+}
